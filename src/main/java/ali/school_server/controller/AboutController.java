eae50b23207721a804dc117af2b239f9a3139588
Muhammadali <132482516+muhammadali-amor@users.noreply.github.com>
2024-10-09 10:48:34 +0500

    about

---
+++ src/main/java/ali/school_server/controller/AboutController.java
@@ -0,0 +1,32 @@
+package ali.school_server.controller;
+
+import ali.school_server.payload.AboutDto;
+import ali.school_server.payload.ApiResponse;
+import ali.school_server.service.AboutService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@CrossOrigin
+@RestController
+@RequestMapping("/api/about")
+public class AboutController {
+
+    private final AboutService aboutService;
+
+    @GetMapping("/")
+    public HttpEntity<?> getAbout() {
+        List<AboutDto> serviceAbout = aboutService.getAbout();
+        return ResponseEntity.ok(serviceAbout);
+    }
+
+    @PostMapping("/add-about")
+    public HttpEntity<?> addAbout(@RequestBody AboutDto aboutDto) {
+        ApiResponse<?> addAbout = aboutService.addAbout(aboutDto);
+        return ResponseEntity.status(addAbout.isSuccess() ? 200 : 400).body(addAbout);
+    }
+}
